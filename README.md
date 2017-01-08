19fbdce67b7a500bc0353c723cfd3068d6b17c74
Bill Kremer <bkrems12@gmail.com>
2017-01-07 20:36:21 -0600

    page works, but needs more formatting before adding additional components.

--- README.md
+++ README.md
@@ -2,6 +2,13 @@
 
 If you want, you can start your project by forking and cloning this repo.
 
-However, feel free to start from scratch,too.
+However, feel free to start from scratch, too.
 
 **REMEMBER:** Carefully read the instructions in the assignment app.
+
+# Salary Input form with Monthly Salary Expenditure Calculation
+
+This repository is an exercise in using jQuery to populate a webpage with employee data using form entry fields then calculating the monthly salary expenditures by totalling the annual salaries of all entered employees and dividing by twelve (12).
+
+## Author: Bill Kremer ##
+### Publication Date: Jan 8, 2017 ##
--- app.js
+++ app.js
@@ -1,17 +1,22 @@
 $(function () {
   console.log('document is ready');
+    var totalAnnualSalary = 0;
 
   $('form').on('submit', function (event) {
-    console.log('form submitted');
-
     event.preventDefault();
+
 
     var formData = {};
     var formAsArray = $(this).serializeArray();
-
+    console.log($(this).serializeArray());
     formAsArray.forEach(function (input) {
       formData[input.name] = input.value;
     });
+
+    totalAnnualSalary += parseFloat(formData.employeeAnnualSalary);
+    console.log(totalAnnualSalary);
+    console.log(parseFloat(formData.employeeAnnualSalary));
+    appendMonthlyExpenditure(totalAnnualSalary);
 
     appendDom(formData);
 
@@ -19,15 +24,50 @@ $(function () {
   });
 });
 
+
+
+
+function appendMonthlyExpenditure(annSalExp) {
+
+  var $totalAnnualSalaries = annSalExp;
+
+  $('#monthlySalary').find('h2').text("Monthly Salary Expenditure: $" + (Math.round( parseFloat($totalAnnualSalaries)*100/12))/100 );
+};
+
+
+
 function appendDom(emp) {
-  var $emp = $('<div class="employee"></div>'); // create a div jQuery object
+  var $empFName = $('<div class="employee"></div>');
+  var $empLName = $('<div class="employee"></div>');
+  var $empID = $('<div class="employee"></div>');
+  var $empJobTitle = $('<div class="employee"></div>');
+  var $empAnnualSalary = $('<div class="employee"></div>');
 
-  $emp.append('<p>' + emp.employeeFirstName + ' ' + emp.employeeLastName + '</p>'); // add our employee data
-  $emp.append('<p>' + emp.employeeIdNumber + '</p>');
 
-  $('#employees').append($emp); // append our div to the DOM
+//  var $emp = $('<div class="employee"></div>'); // create a div jQuery object
+
+  $empFName.append('<p>' + emp.employeeFirstName + '</p>');
+  $empLName.append('<p>' + emp.employeeLastName + '</p>');
+  $empID.append('<p>' + emp.employeeIdNumber + '</p>');
+  $empJobTitle.append('<p>' + emp.employeeJobTitle + '</p>');
+  $empAnnualSalary.append('<p>$ ' + emp.employeeAnnualSalary + '</p>');
+
+
+
+//  $emp.append(emp.employeeIdNumber + ' ' + emp.employeeJobTitle + ' ');
+//  $emp.append(emp.employeeAnnualSalary)
+
+//  $('#employees').append($emp); // append our div to the DOM
+    $('#employees').find('.FName').append($empFName);
+    $('#employees').find('.LName').append($empLName);
+    $('#employees').find('.ID').append($empID);
+    $('#employees').find('.Title').append($empJobTitle);
+    $('#employees').find('.AnnSal').append($empAnnualSalary);
+
+
 }
 
 function clearForm() {
   $('form').find('input[type=text]').val('');
+  $('form').find('input[type=number]').val('');
 }
