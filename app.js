cd3a953e4f4ae4bb8b3aa04668e818a8b6fdea53
Bill Kremer <bkrems12@gmail.com>
2017-01-08 00:08:29 -0600

    completed Hard level

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 $(function () {
-  console.log('document is ready');
-    var totalAnnualSalary = 0;
+//  console.log('document is ready');
+  var totalAnnualSalary = 0;
 
   $('form').on('submit', function (event) {
     event.preventDefault();
@@ -8,66 +8,73 @@ $(function () {
 
     var formData = {};
     var formAsArray = $(this).serializeArray();
-    console.log($(this).serializeArray());
+
     formAsArray.forEach(function (input) {
       formData[input.name] = input.value;
     });
 
+    formData.employeeSecretID = Math.floor((Math.random() * 10000000) + 1);
+    // an identifier for deleting data later.
+    console.log(formData.employeeSecretID);
+
+    if (isNaN(parseFloat(formData.employeeAnnualSalary))) {
+      formData.employeeAnnualSalary = 0;
+    } // ensures enployeeAnnualSalary is a number.
+
     totalAnnualSalary += parseFloat(formData.employeeAnnualSalary);
-    console.log(totalAnnualSalary);
-    console.log(parseFloat(formData.employeeAnnualSalary));
     appendMonthlyExpenditure(totalAnnualSalary);
 
     appendDom(formData);
 
     clearForm();
   });
+
+
+  $('#employees').click('button', function()  {
+    var temp = $(this).find('.employee').attr('class').split(' ')[1];
+    // searches up the DOM and returns the secretID of the button pressed
+    $("." + temp).remove(); // removes everything with the secretID class!
+  });
+
 });
 
 
 
-
 function appendMonthlyExpenditure(annSalExp) {
-
+  // function for appending the Monthly Salary in the Dom
   var $totalAnnualSalaries = annSalExp;
 
   $('#monthlySalary').find('h2').text("Monthly Salary Expenditure: $" + (Math.round( parseFloat($totalAnnualSalaries)*100/12))/100 );
 };
 
 
-
 function appendDom(emp) {
-  var $empFName = $('<div class="employee"></div>');
-  var $empLName = $('<div class="employee"></div>');
-  var $empID = $('<div class="employee"></div>');
-  var $empJobTitle = $('<div class="employee"></div>');
-  var $empAnnualSalary = $('<div class="employee"></div>');
+  var $empFName = $('<div class="employee '+ emp.employeeSecretID  +'"></div>');
+  var $empLName = $('<div class="employee '+ emp.employeeSecretID  +'"></div>');
+  var $empID = $('<div class="employee '+ emp.employeeSecretID  +'"></div>');
+  var $empJobTitle = $('<div class="employee '+ emp.employeeSecretID  +'"></div>');
+  var $empAnnualSalary = $('<div class="employee '+ emp.employeeSecretID  +'"></div>');
+  var $empDeleteButton = $('<div class="employee '+ emp.employeeSecretID  +'"></div>');
 
-
-//  var $emp = $('<div class="employee"></div>'); // create a div jQuery object
 
   $empFName.append('<p>' + emp.employeeFirstName + '</p>');
   $empLName.append('<p>' + emp.employeeLastName + '</p>');
   $empID.append('<p>' + emp.employeeIdNumber + '</p>');
   $empJobTitle.append('<p>' + emp.employeeJobTitle + '</p>');
   $empAnnualSalary.append('<p>$ ' + emp.employeeAnnualSalary + '</p>');
+  $empDeleteButton.append('<p><button>Delete</button></p>');
 
 
+  $('#employees').find('.FName').append($empFName);
+  $('#employees').find('.LName').append($empLName);
+  $('#employees').find('.ID').append($empID);
+  $('#employees').find('.Title').append($empJobTitle);
+  $('#employees').find('.AnnSal').append($empAnnualSalary);
+  $('#employees').find('.DelButton').append($empDeleteButton);
+};
 
-//  $emp.append(emp.employeeIdNumber + ' ' + emp.employeeJobTitle + ' ');
-//  $emp.append(emp.employeeAnnualSalary)
-
-//  $('#employees').append($emp); // append our div to the DOM
-    $('#employees').find('.FName').append($empFName);
-    $('#employees').find('.LName').append($empLName);
-    $('#employees').find('.ID').append($empID);
-    $('#employees').find('.Title').append($empJobTitle);
-    $('#employees').find('.AnnSal').append($empAnnualSalary);
-
-
-}
 
 function clearForm() {
   $('form').find('input[type=text]').val('');
-  $('form').find('input[type=number]').val('');
+  $('form').find('input[type=number]').val(null);
 }
